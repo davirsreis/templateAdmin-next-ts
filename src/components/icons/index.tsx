21977ea9aa0587388c157b47b62ab3b630a997ec
davirsreis <daviresende.2312@gmail.com>
2023-10-19 17:22:02 -0300

    Tela de autenticação

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -34,6 +34,20 @@ export const IconeLua = (tamanho = 6) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-${tamanho} h-${tamanho}`}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
   </svg>
+)
+
+export const IconeAtencao = (tamanho = 6) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-${tamanho} h-${tamanho}`}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+  </svg>
 
 )
+
+
+
+
+export const IconeGoogle = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="none" id="google"><path fill="#fff" d="M0 24C0 10.745 10.745 0 24 0s24 10.745 24 24-10.745 24-24 24S0 37.255 0 24Z"></path><path fill="#EA4335" d="M24 17.621c1.953 0 3.27.844 4.021 1.549l2.935-2.866C29.154 14.628 26.808 13.6 24 13.6c-4.067 0-7.58 2.334-9.29 5.731l3.362 2.612c.844-2.508 3.178-4.322 5.928-4.322Z"></path><path fill="#4285F4" d="M33.984 24.231c0-.855-.07-1.479-.22-2.126H24v3.86h5.732c-.116.959-.74 2.403-2.127 3.374l3.282 2.542c1.965-1.814 3.097-4.484 3.097-7.65Z"></path><path fill="#FBBC05" d="M18.084 26.057A6.401 6.401 0 0 1 17.737 24c0-.716.127-1.41.335-2.057l-3.363-2.612A10.41 10.41 0 0 0 13.6 24c0 1.675.405 3.259 1.11 4.668l3.374-2.611Z"></path><path fill="#34A853" d="M24 34.4c2.808 0 5.165-.924 6.887-2.52l-3.282-2.541c-.878.612-2.057 1.04-3.605 1.04-2.75 0-5.084-1.815-5.916-4.322l-3.363 2.611C16.43 32.066 19.932 34.4 24 34.4Z"></path></svg>
+)
+
 
--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,12 +1,10 @@
 import Layout from "../components/template/Layout";
-import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
-  const {alternarTema} = useAppData()
 
   return (
     <Layout titulo="Notificações" subtitulo="Aqui aparece as suas notificações">
-      <button onClick={alternarTema}>Alternar tema</button>
+      <h1>Notificações</h1>
     </Layout>
   )
 }
